0d473dfa0a676238756c61222f702ee58cf9603d
dtbuild <dtbuild@rigil>
2026-07-22 14:45:26 +0000

    289b8c5e08f85e8944b15864bf359daa7a582c95 Release 2.0.0 ready
    Sync to source repo @289b8c5e08f85e8944b15864bf359daa7a582c95

--- js/rowReorder.dataTables.min.mjs
+++ js/rowReorder.dataTables.min.mjs
@@ -1,4 +1,4 @@
-/*! RowReorder default styling 2.0.0-beta.1 for DataTables
+/*! RowReorder default styling 2.0.0 for DataTables
  * Copyright (c) SpryMedia Ltd - datatables.net/license
  */
 import DataTable from"datatables.net-dt";import RowReorder from"datatables.net-rowreorder";export default DataTable;
